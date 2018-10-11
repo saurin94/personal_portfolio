feb4f38781429387de4eed1a81f4bb1edaafc27e
Saurin Shah <shah.saurin@husky.neu.edu>
2018-10-11 00:23:21 -0400

    bootstrap.min.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/jquery/dist/jquery.min.js';
-import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+// import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 
 class Experience extends Component {
